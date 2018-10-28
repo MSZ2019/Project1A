ffa634c11600e4a32de7107733c3c63d660c869f
msz.icm@gmail.com <mszicmgmailcom@macbook-pro-de-mszicmgmailcom.local>
2018-10-27 22:23:51 -0300

    Initial Commit

---
+++ AplicacionMarco/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AplicacionMarco
+//
+//  Created by msz.icm@gmail.com on 27-10-18.
+//  Copyright © 2018 msz.icm@gmail.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
